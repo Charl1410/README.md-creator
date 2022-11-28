8bd22f13ee24e691295c480ab8df8f0054cb3c49
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2022-11-28 14:10:04 +0000

    working on list of question

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,17 @@
+const fs = require('fs');
+const questions = require('./questions')
+
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile (fileName, data) {
+    fs.writeFile(fileName, data), (error) =>
+    error ? console.log('error') : console.log('done');
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
-
 // Function call to initialize app
 init();
